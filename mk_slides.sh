7171a54b701832692b87222a94ee5eb7d03dcf36
Joachim Neubert <j.neubert@zbw.eu>
2022-05-04 21:48:28 +0200

    Simplify path

--- mk_slides.sh
+++ mk_slides.sh
@@ -2,4 +2,4 @@
 
 # invoke pandoc to create reveal-js slides from markdown
 
-pandoc ./slides.md --css "./css/reset.css" --css "./css/reveal.css" --output ./index.html
+pandoc ./slides.md --css "css/reset.css" --css "css/reveal.css" --output ./index.html
